c2adc5d799536258dc6b620fc0dbb7d384a1e15e
Elkassemi <elkassemi@outlook.com>
2017-12-25 09:07:46 +0000

    server change

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // 'url': 'mongodb://kelwynn:994971crea@ds161455.mlab.com:61455/ada'
-    'url':"mongodb://kelwynn:994971crea@ds161833.mlab.com:61833/home"
+    'url': 'mongodb://kelwynn:994971crea@ds161455.mlab.com:61455/ada'
+    // 'url':"mongodb://kelwynn:994971crea@ds161833.mlab.com:61833/home"
 }
