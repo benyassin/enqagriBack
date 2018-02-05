c747f246b0fb94cac360f796159a7b93590153cc
Elkassemi <elkassemi@outlook.com>
2018-02-05 17:29:24 +0000

    upload file

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ var bodyParser = require('body-parser');
 var compression = require('compression')
 var cors = require('cors');
 var path    = require("path");
+var fs = require('fs');
+var multer = require('multer');
+
 
 var databaseConfig = require('./config/database');
 var router = require('./app/routes');
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(databaseConfig.url,{
@@ -31,5 +35,43 @@ app.use('/angular', express.static(path.join(__dirname + '/angular')));
 
 
 
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('file');
+
+// app.use(multer({
+//     dest: DIR,
+//     rename: function (fieldname, filename) {
+//       return filename + Date.now();
+//     },
+//     onFileUploadStart: function (file) {
+//       console.log(file.originalname + ' is starting ...');
+//     },
+//     onFileUploadComplete: function (file) {
+//       console.log(file.fieldname + ' uploaded to  ' + file.path);
+//     }
+//   }));
+
+   
+app.post('/upload', function(req, res) {
+    upload(req,res,function(err){
+        console.log(req.file);
+        if(err){
+             res.json({error_code:1,err_desc:err});
+             return;
+        }
+         res.json({error_code:0,err_desc:null});
+    });
+});
 
 router(app);
